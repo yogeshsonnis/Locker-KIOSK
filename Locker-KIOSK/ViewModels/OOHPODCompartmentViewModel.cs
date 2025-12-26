8589592445e486576c510fa4e032f5d5f2b25afd
Yogesh <silverlightyogesh@gmail.com>
2025-12-26 19:46:56 +0530

    Created Button Commands

--- Locker-KIOSK/ViewModels/OOHPODCompartmentViewModel.cs
+++ Locker-KIOSK/ViewModels/OOHPODCompartmentViewModel.cs
@@ -13,12 +13,41 @@ namespace Locker_KIOSK.ViewModels
     {
         private readonly MainViewModel _mainVM;
        
-
         public OOHPODCompartmentViewModel(MainViewModel mainVM)
         {
-            _mainVM = mainVM;    
+            _mainVM = mainVM;
+
+            ReOpenCommand = new RelayCommand(_ => ReOpen());
+            ReportIssueCommand = new RelayCommand(_ => ReportIssue());
+            OpenBiggerCommand = new RelayCommand(_ => OpenBigger());
+            DoneCommand = new RelayCommand(_ => Done());
+        }
+
+        private void Done()
+        {
+           
+        }
+
+        private void OpenBigger()
+        {
+           
+        }
+
+        private void ReportIssue()
+        {
             
         }
+
+        private void ReOpen()
+        {
+            
+        }
+
+        public ICommand ReOpenCommand { get; }
+        public ICommand ReportIssueCommand { get; }
+        public ICommand OpenBiggerCommand { get; }
+        public ICommand DoneCommand { get; }
+
         
     }
 }
